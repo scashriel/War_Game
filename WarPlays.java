9e701857145bf5a960325c6ca43b78476116df6e
Shirah Cashriel <scashriel@gmail.com:wq>
2020-01-07 20:45:02 +0200

    created a function to remove duplicate code

--- WarPlays.java
+++ WarPlays.java
@@ -44,27 +44,18 @@ public class WarPlays {
         d1.removeFromDeck(c1);
         warDeck.addToDeck(c2);
         d2.removeFromDeck(c2);
+        boolean playStatus = false;
 
         int max = c1.getMax(c2);
         //player 1 wins round
         if (max == 1){
-            d1.moveCards(warDeck, warDeck.getDeckSize());
-            JFrame card1 = drawCardImage(one, winBattle, c1, xSize1, ySize);
-            JFrame card2 = drawCardImage(two, loseBattle, c2, xSize2, ySize);
-            JOptionPane.showMessageDialog(null, "\nPlayer One takes the pile!\n");
-            card1.dispose();
-            card2.dispose();
-            return true;
+            winningRound(d1, warDeck, one, c1, xSize1, two, c2, xSize2);
+            playStatus = true;
         }
         //player 2 wins
         else if (max == -1){
-            d2.moveCards(warDeck, warDeck.getDeckSize());
-            JFrame card1 = drawCardImage(one, loseBattle, c1, xSize1, ySize);
-            JFrame card2 = drawCardImage(two, winBattle, c2, xSize2, ySize);
-            JOptionPane.showMessageDialog(null, "\nPlayer Two takes the pile!\n");
-            card1.dispose();
-            card2.dispose();
-            return true;
+            winningRound(d2, warDeck, two, c2, xSize2, one, c1, xSize1);
+            playStatus = true;
         }
         //it's a tie
         else{
@@ -74,26 +65,38 @@ public class WarPlays {
             card1.dispose();
             card2.dispose();
             if (d1.getDeckSize() < 3){
-                card1 = drawCardImage(one, loseWar, new Card("lose"), xSize1, ySize);
-                card2 = drawCardImage(two,winWar, new Card ("win"), xSize2, ySize);
-                JOptionPane.showMessageDialog(null, "GAME OVER");
-                card1.dispose();
-                card2.dispose();
-                return false;
+                gameOver(two, one, xSize2, xSize1);
+                playStatus = false;
             }
             else if (d2.getDeckSize() < 3){
-                card1 = drawCardImage(one, winWar, new Card("win"), xSize1, ySize);
-                card2 = drawCardImage(two, loseWar, new Card("lose"), xSize2, ySize);
-                JOptionPane.showMessageDialog(null, "GAME OVER");
-                card1.dispose();
-                card2.dispose();
-                return false;
+                gameOver(one, two, xSize1, xSize2);
+                playStatus = false;
             }
             else {
                 warRound(c1, c2, d1, d2, warDeck, xSize1, xSize2, ySize);
-                return true;
+                playStatus = true;
             }
         }
+        return playStatus;
+    }
+
+
+    public void winningRound(DeckOfCards d, DeckOfCards warDeck, String winner, Card winCard, int winX,
+                             String loser, Card loseCard, int loseX){
+        d.moveCards(warDeck, warDeck.getDeckSize());
+        JFrame cardWin = drawCard(winner, winBattle, winCard, winX, ySize);
+        JFrame cardLose = drawCard(loser, loseBattle, loseCard, loseX, ySize);
+        JOptionPane.showMessageDialog(null, "\nPlayer"+ winner+ "takes the pile!\n");
+        cardWin.dispose();
+        cardLose.dispose();
+    }
+
+    public void gameOver(String winner, String loser, int winX, int loseX){
+        JFrame winCard = drawCardImage(winner, winWar, new Card("win"), winX, ySize);
+        JFrame loseCard = drawCardImage(loser, loseWar, new Card("lose"), loseX, ySize);
+        JOptionPane.showMessageDialog(null, "GAME OVER");
+        winCard.dispose();
+        loseCard.dispose();
     }
 
     public void warRound(Card c1, Card c2, DeckOfCards d1, DeckOfCards d2, DeckOfCards warDeck, int xSize1, int xSize2, int ySize){
